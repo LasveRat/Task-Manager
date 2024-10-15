fa1341d9c4eb9da7d7da64986363c6cd3b169738
Lasve <johnvella2001@gmail.com>
2024-10-15 23:14:53 +0300

    push

--- backend/server.js
+++ backend/server.js
@@ -4,13 +4,14 @@ import dotenv from "dotenv";
 import cors from "cors";
 import connect from "./src/db/connect.js";
 import cookieParser from "cookie-parser";
+import fs  from "node:fs";
 
 
 dotenv.config();
 
 const port = process.env.PORT || 8000;
 const app = express();
-app.use(cookieParser());
+
 
 // middleware 
 app.use(cors({
@@ -19,7 +20,19 @@ app.use(cors({
 }));
 app.use(express.json());
 app.use(express.urlencoded({extended: true }));
+app.use(cookieParser());
 
+
+//routes 
+const routeFiles = fs.readdirSync("./src/routes");
+routeFiles.forEach((file)=> {
+    //use dynamic import 
+    import(`./src/routes/${file}`).then((route) => {
+        app.use("/api/v1" , route.default);
+    }).catch((err) =>{
+        console.log("Failed to load route file" , err);
+    });
+});
 
 
 const server = async () => {
