3c04c8ccfa63cbfe7ab7fdd4595622084771ce36
Lasve <johnvella2001@gmail.com>
2024-10-29 15:44:01 +0200

    feat: get users

--- backend/src/controllers/auth/adminController.js
+++ backend/src/controllers/auth/adminController.js
@@ -14,3 +14,17 @@ export const deleteUser = asyncHandler(async (req , res) =>{
         res.status(500).json({message:"Could not delete user!"});
     }
 });
+
+// get all users
+export const getAllUsers = asyncHandler(async (req , res) =>{
+    
+    try {
+        const users = await User.find({});
+        if(!users){
+            res.status(404).json({message:"No users found!"});
+        }
+        res.status(200).json(users);
+    } catch (error) {
+        res.status(500).json({message:"Could not get users!"});
+    }
+});
--- backend/src/routes/UserRoutes.js
+++ backend/src/routes/UserRoutes.js
@@ -6,8 +6,8 @@ import {
     registerUser,
     updateUser,
 } from "../controllers/auth/userController.js";
-import { adminMiddleware, protect } from "../middleware/authMiddleware.js";
-import { deleteUser } from "../controllers/auth/adminController.js";
+import { adminMiddleware, creatorMiddleware, protect } from "../middleware/authMiddleware.js";
+import { deleteUser, getAllUsers } from "../controllers/auth/adminController.js";
 
 
 const router = express.Router();
@@ -23,7 +23,8 @@ router.patch("/user" , protect , updateUser);
 
 router.delete("/admin/user/:id" , protect , adminMiddleware , deleteUser);
 
-
+// get all users
+router.get("/admin/users" , protect , creatorMiddleware , getAllUsers);
 
 
 export default router;
